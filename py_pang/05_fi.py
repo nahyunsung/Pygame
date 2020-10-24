bf2157f73c2ee748c7b8baa2aebb4f5d72f285ab
nahyunsung <rahs0120@gmail.com>
2020-10-24 14:12:41 +0900

    1. 파이게임 초기화

---
+++ py_pang/05_fi.py
@@ -0,0 +1,33 @@
+import pygame
+import os
+
+# 1. 파이게임 초기화
+# 2. 게임 환경 설정
+# 3. 캐릭터 이동
+# 4. 무기 발사
+# 5. 공 추가
+# 6. 충돌 처리
+# 7. 공 나누기
+# 8. 메시지 출력
+
+# 파이게임 초기화
+pygame.init()
+
+screen_width = 640
+screen_height = 480
+screen_size = (screen_width, screen_height)
+screen = pygame.display.set_mode(screen_size)
+pygame.display.set_caption("Pypang!!")
+
+# 게임 루프
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # 화면 업데이트
+    pygame.display.update()
+
+# 파이게임 종료
+pygame.quit()
